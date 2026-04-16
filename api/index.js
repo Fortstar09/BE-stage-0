fa0e4342c49b1c5333136e682e47d37c2928d435
Fortstar09 <ogunyafort@gmail.com>
2026-04-16 13:48:57 +0100

    fix: rest to fix build error

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ export default function handler(req, res) {
   const origin = req.headers.origin;
 
   res.setHeader("Access-Control-Allow-Origin", origin);
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Methods", "GET");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
   if (req.method === "GET") {
